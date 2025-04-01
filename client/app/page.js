ed528ac00c4061cd6315d3f04734f1f20093ea73
s4lvi <salvicomputers@gmail.com>
2025-04-01 15:01:50 -0400

    updates

--- client/app/page.js
+++ client/app/page.js
@@ -38,6 +38,29 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {/* Added external link */}
+      <a
+        target="_blank"
+        href="https://jam.pieter.com"
+        style={{
+          fontFamily: "system-ui, sans-serif",
+          position: "fixed",
+          bottom: "-1px",
+          right: "-1px",
+          padding: "7px",
+          fontSize: "14px",
+          fontWeight: "bold",
+          background: "#fff",
+          color: "#000",
+          textDecoration: "none",
+          zIndex: 10000,
+          borderTopLeftRadius: "12px",
+          border: "1px solid #fff",
+        }}
+      >
+        🕹️ Vibe Jam 2025
+      </a>
     </main>
   );
 }
